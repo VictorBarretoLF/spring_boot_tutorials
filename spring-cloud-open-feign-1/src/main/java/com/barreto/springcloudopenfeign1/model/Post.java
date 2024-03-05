cf9668df0887e5a15fe327db8a594e898b9aff14
Víctor Barreto <victor.barreto@behoh.com>
2024-03-05 14:54:25 -0300

    feat: adicionado Model Post

--- spring-cloud-open-feign-1/src/main/java/com/barreto/springcloudopenfeign1/model/Post.java
+++ spring-cloud-open-feign-1/src/main/java/com/barreto/springcloudopenfeign1/model/Post.java
@@ -1,5 +1,17 @@
 package com.barreto.springcloudopenfeign1.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Post {
 
+    private String userId;
+    private Long id;
+    private String title;
+    private String body;
+    
 }
